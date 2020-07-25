7a72c53712788b3d8b10b393dfa30cbe5d3a8686
John Meyer <john@JohnMeyrsLaptop.verizon.net>
2020-07-24 23:02:10 -0400

    Added support for record pages

--- .sfdx/typings/lwc/apex/GetCommunityURL.d.ts
+++ .sfdx/typings/lwc/apex/GetCommunityURL.d.ts
@@ -1,3 +1,6 @@
 declare module "@salesforce/apex/GetCommunityURL.getCommunityURL" {
   export default function getCommunityURL(): Promise<any>;
 }
+declare module "@salesforce/apex/GetCommunityURL.getTwitterScreenName" {
+  export default function getTwitterScreenName(param: {recordId: any, objectApiName: any, fieldApiName: any}): Promise<any>;
+}
--- force-app/main/default/lwc/twitterFeed/twitterFeed.js
+++ force-app/main/default/lwc/twitterFeed/twitterFeed.js
@@ -1,18 +1,40 @@
-import { LightningElement, api, wire, track } from "lwc";
+import { LightningElement, api } from "lwc";
 import getCommunityUrl from "@salesforce/apex/GetCommunityURL.getCommunityURL";
+import getTwitterScreenName from "@salesforce/apex/GetCommunityURL.getTwitterScreenName";
 
 export default class TwitterFeed extends LightningElement {
 	@api cardTitle = "Twitter Feed";
 	@api screenName = "salesforce";
 	@api height = 500;
 	@api theme = "light";
-	@track twitterVfUrl = "";
+	@api recordId;
+	@api objectApiName;
+	@api fieldApiName;
+	twitterVfUrl = "";
 
 	renderedCallback() {
-		getCommunityUrl().then((result) => {
-			console.log(`getCommunityUrl: ${JSON.stringify(result)}`);
-			let communityUrl = result ? result : "";
-			this.twitterVfUrl = `${communityUrl}/apex/TwitterFeed?screenname=${this.screenName}?theme=${this.theme}`;
-		});
+		if (this.recordId != undefined) {
+			console.log(`Record ID: ${this.recordId}`);
+			console.log(`Object API: ${this.objectApiName}`);
+			console.log(`Field API: ${this.fieldApiName}`);
+			getTwitterScreenName({
+				recordId: this.recordId,
+				objectApiName: this.objectApiName,
+				fieldApiName: this.fieldApiName
+			}).then((result) => {
+				if (result === null) {
+					this.screenName = "salesforce";
+				} else {
+					this.screenName = result;
+				}
+				getCommunityUrl().then((result) => {
+					this.twitterVfUrl = `${result}/apex/TwitterFeed?screenname=${this.screenName}?theme=${this.theme}`;
+				});
+			});
+		} else {
+			getCommunityUrl().then((result) => {
+				this.twitterVfUrl = `${result}/apex/TwitterFeed?screenname=${this.screenName}?theme=${this.theme}`;
+			});
+		}
 	}
 }
