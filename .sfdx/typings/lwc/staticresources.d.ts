4d0482ae3d97a0ae78c7ce5b47423c7c64c264e6
John Meyer <john@JohnMeyrsLaptop.verizon.net>
2020-07-25 20:27:22 -0400

    Updated image

--- .sfdx/typings/lwc/staticresources.d.ts
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -1,12 +1,4 @@
-declare module "@salesforce/resourceUrl/TwitterFeed_Icons" {
-    var TwitterFeed_Icons: string;
-    export default TwitterFeed_Icons;
-}
 declare module "@salesforce/resourceUrl/TwitterIcon" {
     var TwitterIcon: string;
     export default TwitterIcon;
-}
-declare module "@salesforce/resourceUrl/twitterFeed_Icons" {
-    var twitterFeed_Icons: string;
-    export default twitterFeed_Icons;
 }
